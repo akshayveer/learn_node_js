4537bfc5b7fd265e698a330cde5d669a67b803b7
akshayveer <akshayveer096@gmail.com>
2017-11-02 19:37:55 +0530

    Error handling with bad address

--- weather-app/app.js
+++ weather-app/app.js
@@ -24,9 +24,13 @@ request({
 	json : true
 }, (error, response, body) => {
 	if (error) {
-		console.log(error);
+		console.log('unable to connect to google servers');
+	} else if (body.status === 'ZERO_RESULTS') {
+		console.log('unable to find the address');
+	} else if (body.status === 'OK') {
+		console.log(body.results[0].formatted_address);
+		var location = body.results[0].geometry.location;
+		console.log(`Latitude : ${location.lat}, Longitude : ${location.lng}`)
 	}
-	console.log(body.results[0].formatted_address);
-	var location = body.results[0].geometry.location;
-	console.log(`Latitude : ${location.lat}, Longitude : ${location.lng}`)
+	
 });
