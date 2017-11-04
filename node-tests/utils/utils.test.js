70c922393a7e31379bb7fb4a105f4a42b4db3c62
akshayveer <akshayveer096@gmail.com>
2017-11-04 16:58:32 +0530

    assertion library

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,23 @@
 const utils = require('./utils')
+const expect = require('expect')
 
 it('should add two number', () => {
 	var res = utils.add(1, 9);
-	if (res !== 10) {
+	expect(res).toBe(10, `Expected 11 but got, ${res}`)
+	/*if (res !== 10) {
 		throw new Error(`expected 10 but got,${res}`);
-	} 
+	} */
 });
 
 it('should square a number', () => {
 	var res = utils.square(9);
-	if (res != 81) {
-		throw new Error(`expected 81 but got ${res}`); 
-	}
-})
+	expect(res).toBe(81, `Expected 81 but got ${res}`).toBeA('number');
+	// if (res != 81) {
+	// 	throw new Error(`expected 81 but got ${res}`); 
+	// }
+});
+
+it('shoudl be equal', () => {
+	// expect({name: 'Akshay'}).toBe({name: 'Akshay'}); // faiils
+	expect({name : 'Akshay'}).toEqual({name: 'Akshay'});
+});
