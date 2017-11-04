635851e03a766437835d2c5df9ff8f117ccbde5a
akshayveer <akshayveer096@gmail.com>
2017-11-04 12:19:13 +0530

    advanced promises

--- weather-app/playground/promises.js
+++ weather-app/playground/promises.js
@@ -1,3 +1,26 @@
+
+var asyncAdd = (a, b) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if (typeof a === 'number' && typeof b === 'number') {
+				resolve(a + b);
+			} else {
+				reject('One or more argumnets is not number');
+			}
+		}, 1500);
+	});
+}
+
+asyncAdd(1, 'a').then((sum) => {
+	console.log('Result: ', sum);
+	return asyncAdd(sum , 33);
+}). then((sum) => {
+	console.log('Result: ', sum);
+}). catch ((errorMessage) => {
+	console.log(errorMessage);
+});
+
+
 var somePromise = new Promise((resolve, reject) => {
 	resolve('Hey it worked');
 });
